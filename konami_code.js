70743fbf7dffce7bcf3d0ca1748105695fce2ef7
rafaelalfinito@hotmail.com <rafaelalfinito@hotmail.com>
2018-05-05 18:46:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+var i = 0
 function init() {
   // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown',konami)
+}
 
+function konami(e){
+  key = parseInt(e.which)
+  if(key===code[i]){
+    i++
+  } else{
+    i = 0
+  }
+  if(i===code.length){
+    alert()
+  }
 }
